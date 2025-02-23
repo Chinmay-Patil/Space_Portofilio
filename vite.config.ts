f0b3c04872fd3451505310e3927278d3296762f1
ChinmayPatil <patilchinmay62@gmail.com>
2025-02-22 23:05:05 -0500

    Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Space_Portofilio/',
+ base: '/Space_Portofilio/',  // Repository name
+  build: {
+    assetsDir: 'assets',  // Customize the asset directory
+  },
   plugins: [react()],
 })
