bd595b30e95ef4850cdca52833e351af59d4396b
ChinmayPatil <patilchinmay62@gmail.com>
2025-02-23 18:46:55 -0500

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   build: {
     assetsDir: 'assets',  // Customize the asset directory
     //  minify: "terser", // Alternative minifier that may handle eval better
