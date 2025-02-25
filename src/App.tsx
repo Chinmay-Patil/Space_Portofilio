63144e9670970c643bb1954c207937bf93117032
ChinmayPatil <patilchinmay62@gmail.com>
2025-02-24 23:53:03 -0500

    removed effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { Canvas } from '@react-three/fiber';
 import { Model } from './models/earth';
 import { Stars } from '@react-three/drei';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from 'react-router-dom';
-import { Suspense, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Suspense } from 'react';
 import { AboutScene } from './scenes/aboutScene';
 import { KnowledgeScene } from './scenes/knowledgeScene';
 import { ProjectScene } from './scenes/projectsScene';
@@ -15,23 +10,6 @@ import { ConatactScene } from './scenes/contactScene';
 import { Loader } from './components/loader';
 
 function Scene() {
-  const location = useLocation();
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    // Only apply loading state for root route
-    if (location.pathname === '/') {
-      setIsLoading(true);
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 16000);
-
-      return () => clearTimeout(timer);
-    } else {
-      // For other routes, set loading to false immediately
-      setIsLoading(false);
-    }
-  }, [location]);
-
   return (
     <>
       <color attach="background" args={['#000000']} />
@@ -46,19 +24,15 @@ function Scene() {
         speed={1}
       />
 
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<Model />} />
-            <Route path="/about" element={<AboutScene />} />
-            <Route path="/knowledge" element={<KnowledgeScene />} />
-            <Route path="/projects" element={<ProjectScene />} />
-            <Route path="/contact" element={<ConatactScene />} />
-          </Routes>
-        </Suspense>
-      )}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Model />} />
+          <Route path="/about" element={<AboutScene />} />
+          <Route path="/knowledge" element={<KnowledgeScene />} />
+          <Route path="/projects" element={<ProjectScene />} />
+          <Route path="/contact" element={<ConatactScene />} />
+        </Routes>
+      </Suspense>
 
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
